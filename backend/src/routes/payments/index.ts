50a8e277b7a0317a221364b9467d4af7baf1d19c
AMAN SINGH <amansingh33849@gmail.com>
2024-09-18 20:49:19 +0000

    transactions dashboard

--- backend/src/routes/payments/index.ts
+++ backend/src/routes/payments/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { depositMoney, withdrawMoney } from "../../controllers/payments";
+import { depositMoney, withdrawMoney,transactionHistory } from "../../controllers/payments";
 import { authenticateJWT } from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/deposit-money", authenticateJWT, depositMoney);
 router.post("/withdraw-money", authenticateJWT, withdrawMoney);
-
+router.get("/transaction-history",authenticateJWT,transactionHistory);
 export default router;

255afe9509bcda4258492b25c7a0c2917ced2bfc
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2024-09-19 20:33:40 +0530

    Added transaction status to transaction table

--- backend/src/utils/payment.ts
+++ backend/src/utils/payment.ts
@@ -1,3 +1,5 @@
+import { db } from "../db";
+
 type TUser = {
     id: string;
     name: string | null;
@@ -9,7 +11,19 @@ type TUser = {
 // Function to send money from user -> company account
 export const depositMoneyToCompany = async (amount: number, user: TUser) => {
     try {
+        if (!user || !user?.id) return false;
         console.log("Deposit Money to company", amount, "Received from", user)
+        const status = C2B();
+        await db.transaction.create({
+            data: {
+                user: {
+                    connect: { id: user.id }
+                },
+                amount,
+                type: "DEPOSIT",
+                status: status ? "COMPLETED" : "CANCELLED",
+            }
+        })
         return true;
     } catch (error) {
         console.log("Error in payment to company", error);
@@ -20,10 +34,40 @@ export const depositMoneyToCompany = async (amount: number, user: TUser) => {
 // Function to send money from company -> user
 export const withdrawMoneyToUser = async (amount: number, user: TUser) => {
     try {
+        if (!user || !user?.id) return false;
         console.log("Deposit Money to user", amount, "Received from", user)
+        const status = B2C();
+        await db.transaction.create({
+            data: {
+                user: {
+                    connect: { id: user.id }
+                },
+                amount,
+                type: "WITHDRAWAL",
+                status: status ? "COMPLETED" : "CANCELLED",
+            }
+        })
         return true;
     } catch (error) {
         console.log("Error in payment to user", error);
+        return false;
+    }
+}
+
+function C2B() {
+    try {
+        return true;
+    } catch (error) {
+        console.log("Something went wrong in Customer to Business transaction", error);
+        return false;
+    }
+}
+
+function B2C() {
+    try {
+        return true;
+    } catch (error) {
+        console.log("Something went wrong in Business to Customer transaction", error);
         return false;
     }
 }
